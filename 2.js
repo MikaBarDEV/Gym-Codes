c9aed5f7ded3ce3a5e1cf61218650672cf2fc45c
Mika Bar DEV <89031266+MikaBarDEV@users.noreply.github.com>
2021-11-21 07:48:46 +0000

    Initial commit

---
+++ 2.js
@@ -0,0 +1,27 @@
+module.exports = () =>{
+const fs = require('fs')
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+var array = []
+var loop = 0
+
+for(var loop = 1; loop < 10000; loop++){
+
+  const start = randomNumber(1, 20)
+  const end = randomNumber(1, 20)
+  const output = start - end
+  var log = `${start} - ${end} = ${output}`
+
+  if(output < 6 && output > 0 && !array.join(' ').includes(log)){
+  array.push(log)
+  };
+  
+}
+
+fs.writeFileSync('./2.txt', array.join('\n'))
+
+
+}
---
+++ 3.js
@@ -0,0 +1,28 @@
+module.exports = () =>{
+const fs = require('fs')
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+var array = []
+var loop = 0
+
+for(var loop = 1; loop < 10000; loop++){
+
+  const start = randomNumber(1, 20)
+  const second = randomNumber(1, 20)
+  const end = randomNumber(1, 20)
+  const output = start + second - end
+  var log = `${start} + ${second} - ${end} = ${output}`
+
+  if(output < 6 && output > 0 && !array.join(' ').includes(log)){
+  array.push(log)
+  };
+  
+}
+
+fs.writeFileSync('./3.txt', array.join('\n'))
+
+
+}
